e590ce913ed0405f2bb0cddee70842495ddd0128
Philippe Marschall <philippe.marschall@gmail.com>
2015-03-09 21:03:37 +0100

    indentation

--- src/main/java/com/github/marschall/jdtavailabilitycheck/AvailabilityBean.java
+++ src/main/java/com/github/marschall/jdtavailabilitycheck/AvailabilityBean.java
@@ -9,195 +9,195 @@ import javax.servlet.jsp.PageContext;
 
 public class AvailabilityBean {
 
-    private static final String JDT = "org.eclipse.jdt.core.compiler.batch.BatchCompiler";
+  private static final String JDT = "org.eclipse.jdt.core.compiler.batch.BatchCompiler";
 
-    private static final String JDT_INTERNAL = "org.eclipse.jdt.internal.compiler.Compiler";
+  private static final String JDT_INTERNAL = "org.eclipse.jdt.internal.compiler.Compiler";
 
-    private static final String JSR_199 = "org.apache.jasper.compiler.Jsr199JavaCompiler";
+  private static final String JSR_199 = "org.apache.jasper.compiler.Jsr199JavaCompiler";
 
-    private static final String LOG4J_LOGGER = "org.apache.logging.log4j.Logger";
+  private static final String LOG4J_LOGGER = "org.apache.logging.log4j.Logger";
 
-    private static final String LOG4J_LOGMANAGER = "org.apache.logging.log4j.LogManager";
+  private static final String LOG4J_LOGMANAGER = "org.apache.logging.log4j.LogManager";
 
-    private static final String SLF4J_LOGGER = "org.slf4j.Logger";
+  private static final String SLF4J_LOGGER = "org.slf4j.Logger";
 
-    private static final String SLF4J_LOGGER_FACTORY = "org.slf4j.LoggerFactory";
+  private static final String SLF4J_LOGGER_FACTORY = "org.slf4j.LoggerFactory";
 
-    private static final String JCL_LOG = "org.apache.commons.logging.Log";
+  private static final String JCL_LOG = "org.apache.commons.logging.Log";
 
-    private static final String JCL_LOG_FACTORY = "org.apache.commons.logging.LogFactory";
+  private static final String JCL_LOG_FACTORY = "org.apache.commons.logging.LogFactory";
 
-    private HttpServletRequest request;
+  private HttpServletRequest request;
 
-    private HttpServletResponse response;
+  private HttpServletResponse response;
 
-    private HttpSession session;
+  private HttpSession session;
 
-    private ServletContext servletContext;
+  private ServletContext servletContext;
 
-    private ServletConfig config;
+  private ServletConfig config;
 
-    private PageContext pageContext;
+  private PageContext pageContext;
 
-    public void setServletRequest(HttpServletRequest request)  {
-        this.request = request;
+  public void setServletRequest(HttpServletRequest request)  {
+    this.request = request;
+  }
+
+  public String getServletRequestClass() {
+    return this.request.getClass().getName();
+  }
+
+  public boolean isServletRequestClassVisible() {
+    return this.isClassAvailable(this.getServletRequestClass());
+  }
+
+  public void setServletResponse(HttpServletResponse response)  {
+    this.response = response;
+  }
+
+  public String getServletResponseClass() {
+    return this.response.getClass().getName();
+  }
+
+  public boolean isServletResponseClassVisible() {
+    return this.isClassAvailable(this.getServletResponseClass());
+  }
+
+  public void setHttpSession(HttpSession session)  {
+    this.session = session;
+  }
+
+  public String getHttpSessionClass() {
+    return this.session.getClass().getName();
+  }
+
+  public boolean isHttpSessionClassVisible() {
+    return this.isClassAvailable(this.getHttpSessionClass());
+  }
+
+  public void setServletContext(ServletContext context)  {
+    this.servletContext = context;
+  }
+
+  public String getServletContextClass() {
+    return this.servletContext.getClass().getName();
+  }
+
+  public boolean isServletContextClassVisible() {
+    return this.isClassAvailable(this.getServletContextClass());
+  }
+
+  public void setPageContext(PageContext context)  {
+    this.pageContext = context;
+  }
+
+  public String getPageContextClass() {
+    return this.pageContext.getClass().getName();
+  }
+
+  public boolean isPageContextClassVisible() {
+    return this.isClassAvailable(this.getServletContextClass());
+  }
+
+  public void setServletConfig(ServletConfig config)  {
+    this.config = config;
+  }
+
+  public String getServletConfigClass() {
+    return this.config.getClass().getName();
+  }
+
+  public boolean isServletConfigClassVisible() {
+    return this.isClassAvailable(this.getServletContextClass());
+  }
+
+  public String getJdtCompilerClassName() {
+    return JDT;
+  }
+
+  public boolean isJdtCompilerClassClassVisible() {
+    return this.isClassAvailable(this.getJdtCompilerClassName());
+  }
+
+  public String getJdtInternalCompilerClassName() {
+    return JDT_INTERNAL;
+  }
+
+  public boolean isJdtInternalCompilerClassClassVisible() {
+    return this.isClassAvailable(this.getJdtCompilerClassName());
+  }
+
+  public String getJsr199CompilerClassName() {
+    return JSR_199;
+  }
+
+  public boolean isJsr199CompilerClassClassVisible() {
+    return this.isClassAvailable(this.getJsr199CompilerClassName());
+  }
+
+  public String getLog4jLoggerClassName() {
+    return LOG4J_LOGGER;
+  }
+
+  public boolean isLog4jLoggerClassClassVisible() {
+    return this.isClassAvailable(this.getLog4jLoggerClassName());
+  }
+
+  public String getLog4jLogManagerClassName() {
+    return LOG4J_LOGMANAGER;
+  }
+
+  public boolean isLog4jLogManagerClassClassVisible() {
+    return this.isClassAvailable(this.getLog4jLogManagerClassName());
+  }
+
+  public String getSlf4jLoggerClassName() {
+    return SLF4J_LOGGER;
+  }
+
+  public boolean isSlf4jLoggerClassVisible() {
+    return this.isClassAvailable(this.getSlf4jLoggerClassName());
+  }
+
+  public String getSlf4jLoggerFactoryClassName() {
+    return SLF4J_LOGGER_FACTORY;
+  }
+
+  public boolean isSlf4jLoggerFactoryClassVisible() {
+    return this.isClassAvailable(this.getSlf4jLoggerFactoryClassName());
+  }
+
+  public String getJclLoggerClassName() {
+    return JCL_LOG;
+  }
+
+  public boolean isJclLoggerClassVisible() {
+    return this.isClassAvailable(this.getJclLoggerClassName());
+  }
+
+  public String getJclLoggerFactoryClassName() {
+    return JCL_LOG_FACTORY;
+  }
+
+  public boolean isJclLoggerFactoryClassVisible() {
+    return this.isClassAvailable(this.getJclLoggerFactoryClassName());
+  }
+
+  public boolean isSeNonApiClassVisible() {
+    return this.isClassAvailable("sun.reflect.Reflection");
+  }
+
+  public boolean isJstlPresent() {
+    return this.isClassAvailable("javax.servlet.jsp.jstl.core.Config");
+  }
+
+  private boolean isClassAvailable(String className) {
+    try {
+      Class.forName(className);
+      return true;
+    } catch (ClassNotFoundException e) {
+      return false;
     }
-
-    public String getServletRequestClass() {
-        return this.request.getClass().getName();
-    }
-
-    public boolean isServletRequestClassVisible() {
-        return this.isClassAvailable(this.getServletRequestClass());
-    }
-
-    public void setServletResponse(HttpServletResponse response)  {
-        this.response = response;
-    }
-
-    public String getServletResponseClass() {
-        return this.response.getClass().getName();
-    }
-
-    public boolean isServletResponseClassVisible() {
-        return this.isClassAvailable(this.getServletResponseClass());
-    }
-
-    public void setHttpSession(HttpSession session)  {
-        this.session = session;
-    }
-
-    public String getHttpSessionClass() {
-        return this.session.getClass().getName();
-    }
-
-    public boolean isHttpSessionClassVisible() {
-        return this.isClassAvailable(this.getHttpSessionClass());
-    }
-
-    public void setServletContext(ServletContext context)  {
-        this.servletContext = context;
-    }
-
-    public String getServletContextClass() {
-        return this.servletContext.getClass().getName();
-    }
-
-    public boolean isServletContextClassVisible() {
-        return this.isClassAvailable(this.getServletContextClass());
-    }
-
-    public void setPageContext(PageContext context)  {
-        this.pageContext = context;
-    }
-
-    public String getPageContextClass() {
-        return this.pageContext.getClass().getName();
-    }
-
-    public boolean isPageContextClassVisible() {
-        return this.isClassAvailable(this.getServletContextClass());
-    }
-
-    public void setServletConfig(ServletConfig config)  {
-        this.config = config;
-    }
-
-    public String getServletConfigClass() {
-        return this.config.getClass().getName();
-    }
-
-    public boolean isServletConfigClassVisible() {
-        return this.isClassAvailable(this.getServletContextClass());
-    }
-
-    public String getJdtCompilerClassName() {
-        return JDT;
-    }
-
-    public boolean isJdtCompilerClassClassVisible() {
-        return this.isClassAvailable(this.getJdtCompilerClassName());
-    }
-
-    public String getJdtInternalCompilerClassName() {
-        return JDT_INTERNAL;
-    }
-
-    public boolean isJdtInternalCompilerClassClassVisible() {
-        return this.isClassAvailable(this.getJdtCompilerClassName());
-    }
-
-    public String getJsr199CompilerClassName() {
-        return JSR_199;
-    }
-
-    public boolean isJsr199CompilerClassClassVisible() {
-        return this.isClassAvailable(this.getJsr199CompilerClassName());
-    }
-
-    public String getLog4jLoggerClassName() {
-        return LOG4J_LOGGER;
-    }
-
-    public boolean isLog4jLoggerClassClassVisible() {
-        return this.isClassAvailable(this.getLog4jLoggerClassName());
-    }
-
-    public String getLog4jLogManagerClassName() {
-        return LOG4J_LOGMANAGER;
-    }
-
-    public boolean isLog4jLogManagerClassClassVisible() {
-        return this.isClassAvailable(this.getLog4jLogManagerClassName());
-    }
-
-    public String getSlf4jLoggerClassName() {
-        return SLF4J_LOGGER;
-    }
-
-    public boolean isSlf4jLoggerClassVisible() {
-        return this.isClassAvailable(this.getSlf4jLoggerClassName());
-    }
-
-    public String getSlf4jLoggerFactoryClassName() {
-        return SLF4J_LOGGER_FACTORY;
-    }
-
-    public boolean isSlf4jLoggerFactoryClassVisible() {
-        return this.isClassAvailable(this.getSlf4jLoggerFactoryClassName());
-    }
-
-    public String getJclLoggerClassName() {
-        return JCL_LOG;
-    }
-
-    public boolean isJclLoggerClassVisible() {
-        return this.isClassAvailable(this.getJclLoggerClassName());
-    }
-
-    public String getJclLoggerFactoryClassName() {
-        return JCL_LOG_FACTORY;
-    }
-
-    public boolean isJclLoggerFactoryClassVisible() {
-        return this.isClassAvailable(this.getJclLoggerFactoryClassName());
-    }
-
-    public boolean isSeNonApiClassVisible() {
-        return this.isClassAvailable("sun.reflect.Reflection");
-    }
-
-    public boolean isJstlPresent() {
-        return this.isClassAvailable("javax.servlet.jsp.jstl.core.Config");
-    }
-
-    private boolean isClassAvailable(String className) {
-      try {
-        Class.forName(className);
-        return true;
-      } catch (ClassNotFoundException e) {
-        return false;
-      }
-    }
+  }
 
 }
--- src/main/java/com/github/marschall/jdtavailabilitycheck/ClassLoaderBean.java
+++ src/main/java/com/github/marschall/jdtavailabilitycheck/ClassLoaderBean.java
@@ -12,92 +12,92 @@ import sun.misc.Unsafe;
 public class ClassLoaderBean {
 
 
-    public boolean isCheckPossible() {
-        Method isRegistered = getIsRegisteredMethod();
-        return isRegistered != null
-                && isRegistered.getReturnType() == Boolean.TYPE
-                && Modifier.isStatic(isRegistered.getModifiers());
-    }
+  public boolean isCheckPossible() {
+    Method isRegistered = getIsRegisteredMethod();
+    return isRegistered != null
+        && isRegistered.getReturnType() == Boolean.TYPE
+        && Modifier.isStatic(isRegistered.getModifiers());
+  }
 
-    public boolean isApplicationClassLoaderThreadContextClassLoader() {
-        return this.getApplicationClassLoader() == this.getThreadContextClassLoader();
-    }
+  public boolean isApplicationClassLoaderThreadContextClassLoader() {
+    return this.getApplicationClassLoader() == this.getThreadContextClassLoader();
+  }
 
-    public boolean isApiClassLoadedFromApplication() {
-        return Cookie.class.getClassLoader() == ClassLoaderBean.class.getClassLoader();
-    }
+  public boolean isApiClassLoadedFromApplication() {
+    return Cookie.class.getClassLoader() == ClassLoaderBean.class.getClassLoader();
+  }
 
-    public boolean isSeXmlClassLoadedFromApplication() {
-        return XMLEventFactory.class.getClassLoader() == ClassLoaderBean.class.getClassLoader();
-    }
+  public boolean isSeXmlClassLoadedFromApplication() {
+    return XMLEventFactory.class.getClassLoader() == ClassLoaderBean.class.getClassLoader();
+  }
 
-    public boolean isSeClassLoadedFromApplication() {
-        return Doc.class.getClassLoader() == ClassLoaderBean.class.getClassLoader();
-    }
+  public boolean isSeClassLoadedFromApplication() {
+    return Doc.class.getClassLoader() == ClassLoaderBean.class.getClassLoader();
+  }
 
-    public boolean isSeNonApiClassLoadedFromApplication() {
-        return Unsafe.class.getClassLoader() == ClassLoaderBean.class.getClassLoader();
-    }
+  public boolean isSeNonApiClassLoadedFromApplication() {
+    return Unsafe.class.getClassLoader() == ClassLoaderBean.class.getClassLoader();
+  }
 
-    public String getThreadContextClassLoaderName() {
-        return getThreadContextClassLoader().getClass().getName();
-    }
+  public String getThreadContextClassLoaderName() {
+    return getThreadContextClassLoader().getClass().getName();
+  }
 
-    public boolean isThreadContextClassLoaderParallelCapable() {
-        return isParallelCapable(getThreadContextClassLoader());
-    }
+  public boolean isThreadContextClassLoaderParallelCapable() {
+    return isParallelCapable(getThreadContextClassLoader());
+  }
 
-    public String getApplicationClassLoaderName() {
-        return getApplicationClassLoader().getClass().getName();
-    }
+  public String getApplicationClassLoaderName() {
+    return getApplicationClassLoader().getClass().getName();
+  }
 
-    public boolean isApplicationClassLoaderParallelCapable() {
-        return isParallelCapable(getApplicationClassLoader());
-    }
+  public boolean isApplicationClassLoaderParallelCapable() {
+    return isParallelCapable(getApplicationClassLoader());
+  }
 
-    private ClassLoader getThreadContextClassLoader() {
-        return Thread.currentThread().getContextClassLoader();
-    }
+  private ClassLoader getThreadContextClassLoader() {
+    return Thread.currentThread().getContextClassLoader();
+  }
 
-    private ClassLoader getApplicationClassLoader() {
-        return ClassLoaderBean.class.getClassLoader();
-    }
+  private ClassLoader getApplicationClassLoader() {
+    return ClassLoaderBean.class.getClassLoader();
+  }
 
-    private boolean isParallelCapable(ClassLoader classLoader) {
-        Method isRegisteredMethod = getIsRegisteredMethod();
-        if (isRegisteredMethod == null) {
-            return false;
-        }
+  private boolean isParallelCapable(ClassLoader classLoader) {
+    Method isRegisteredMethod = getIsRegisteredMethod();
+    if (isRegisteredMethod == null) {
+      return false;
+    }
+    try {
+      Object result = isRegisteredMethod.invoke(null, classLoader.getClass());
+      return (Boolean) result;
+    } catch (ReflectiveOperationException e) {
+      return false;
+    }
+  }
+
+  public String getClassUrl() {
+    // .getProtocol()
+    // vfs:/content/jdt-availability-check.war/WEB-INF/classes/com/github/marschall/jdtavailabilitycheck/ClassLoaderBean.class
+    // file:/Users/marschall/Hacking/Tomcat/apache-tomcat-8.0.15/webapps/jdt-availability-check/WEB-INF/classes/com/github/marschall/jdtavailabilitycheck/ClassLoaderBean.class
+    String resourceName = ClassLoaderBean.class.getName().replace('.', '/') + ".class";
+    return getApplicationClassLoader().getResource(resourceName).toString();
+  }
+
+
+  private Method getIsRegisteredMethod() {
+    for (Class<?> clazz : ClassLoader.class.getDeclaredClasses()) {
+      if (clazz.getName().equals("java.lang.ClassLoader$ParallelLoaders")) {
         try {
-            Object result = isRegisteredMethod.invoke(null, classLoader.getClass());
-            return (Boolean) result;
-        } catch (ReflectiveOperationException e) {
-            return false;
+          Method isRegistered = clazz.getDeclaredMethod("isRegistered", Class.class);
+          isRegistered.setAccessible(true);
+          return isRegistered;
+        } catch (NoSuchMethodException e) {
+          return null;
         }
+      }
     }
-
-    public String getClassUrl() {
-      // .getProtocol()
-      // vfs:/content/jdt-availability-check.war/WEB-INF/classes/com/github/marschall/jdtavailabilitycheck/ClassLoaderBean.class
-      // file:/Users/marschall/Hacking/Tomcat/apache-tomcat-8.0.15/webapps/jdt-availability-check/WEB-INF/classes/com/github/marschall/jdtavailabilitycheck/ClassLoaderBean.class
-      String resourceName = ClassLoaderBean.class.getName().replace('.', '/') + ".class";
-      return getApplicationClassLoader().getResource(resourceName).toString();
-    }
-
-
-    private Method getIsRegisteredMethod() {
-        for (Class<?> clazz : ClassLoader.class.getDeclaredClasses()) {
-            if (clazz.getName().equals("java.lang.ClassLoader$ParallelLoaders")) {
-                try {
-                    Method isRegistered = clazz.getDeclaredMethod("isRegistered", Class.class);
-                    isRegistered.setAccessible(true);
-                    return isRegistered;
-                } catch (NoSuchMethodException e) {
-                    return null;
-                }
-            }
-        }
-        return null;
-    }
+    return null;
+  }
 
 }
